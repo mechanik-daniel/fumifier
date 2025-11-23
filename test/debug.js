6a55eebe26502414dfdfa73a0f53e72eed53a3aa
mechanik-daniel <daniel@outburn.co.il>
2025-11-24 01:37:17 +0200

    test: update debug script to test dual assignment

--- test/debug.js
+++ test/debug.js
@@ -19,11 +19,27 @@ void async function () {
   var navigator = new FhirStructureNavigator(generator);
 
   var expression = `
-  InstanceOf: Extension
-  * url = 'http://example.org/fhir/StructureDefinition/SystolicBP'
-  * ($index := 0; 8).extension
-    * url = 'abc' & $string($index)
-    * valueString = 'value' & $string($index + 1)
+InstanceOf: il-core-patient
+* name
+  * given = 'John'
+  * family = 'Doe'
+* gender = 'male'
+* birthDate = '1980-01-01'
+// should create a single entry in identifier array
+// should include system, value, and use
+// since the il-id slice is max=1
+* identifier[il-id].value = '12345'
+* identifier[il-id].use = 'official'
+// add managing organization using dual assignment
+// should create a single object with both reference and display
+// because managingOrganization is max=1
+* managingOrganization.reference = 'Organization/1'
+* managingOrganization.display = 'Test Organization'
+// this dual assignment into generalPractitioner should result in two entries
+// since generalPractitioner is 0..*
+// one entry with reference, one with display
+* generalPractitioner.reference = 'Practitioner/1'
+* generalPractitioner.display = 'Dr. Smith'
 
 `
 ;
@@ -35,8 +51,6 @@ void async function () {
     console.log('Compiling expression...');
     expr = await fumifier(expression, {
       navigator
-    }, {
-      logLevel: 70
     });
     console.log('Expression compiled successfully');
   } catch (e) {
@@ -48,9 +62,16 @@ void async function () {
   var res;
 
   try {
-    res = await expr.evaluate({
-      resourceType: "Patient"
-    }, { logLevel: 50, validationLevel: 30, throwLevel: 13, collectLevel: 70 });
+    res = await expr.evaluate(
+      {
+        resourceType: "Patient"
+      },
+      {
+        // logLevel: 50,
+        // validationLevel: 30,
+        throwLevel: 13,
+        // collectLevel: 70
+      });
     console.log('Expression evaluated successfully');
   } catch (e) {
     console.error('Error evaluating expression:');
